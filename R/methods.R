c84d801a72ef0e59c21e3d6997a1daa8ac88a6b4
illtyd <robert.king@newcastle.edu.au>
2017-05-31 13:58:20 +1000

    plot.starship() default for one.page changed from TRUE to FALSE
    plot.starship reports fitting method in plot

--- R/methods.R
+++ R/methods.R
@@ -29,7 +29,7 @@ cat("Message: ")
 print(object$optim.results$message)
 }
 
-plot.starship <- function(x,data=NULL,ask=FALSE,one.page=TRUE,breaks="Sturges",plot.title="default",...)
+plot.starship <- function(x,data=NULL,ask=FALSE,one.page=FALSE,breaks="Sturges",plot.title="default",...)
 {
 if (plot.title == "default") {
   plot.title <- paste(x$method.name,"fit of",x$param,"type GLD")
@@ -37,14 +37,14 @@ if (plot.title == "default") {
 allpar <- par()
 opar <- allpar[match(c("ask","mfrow"),names(allpar))]
 if (is.null(x$data)){
-	if (is.null(data)) {stop("No data to compare fit to")} 
+	if (is.null(data)) {stop("No data to compare to fit.  Use return.data=TRUE")} 
 } else {
 	if (is.null(data)) {data <- x$data #using data returned by starship function
 		} else { 
 		warning(paste(substitute(x),"has a data element and the data argument was also given.\nUsing ",paste(substitute(data))," instead of the data element of ",substitute(x))) } }
 if (ask) {par(ask=TRUE)}
 if (one.page) {par(mfrow=c(2,1))}
-qqgl(y=data,lambda.pars1=x$lambda,param=x$param,xlab="Fitted Theoretical Quantiles",main=plot.title)
+qqgl(y=data,lambda.pars1=x$lambda,param=x$param,xlab=paste(x$method.name," Fitted Theoretical Quantiles"),main=plot.title) # add which option here
 hist(data,prob=TRUE,xlab="Data",breaks=breaks,main=plot.title,...)
 plotgld(lambda1=x$lambda,param=x$param,new.plot=FALSE,...)
 par(opar) # Return to previous par
