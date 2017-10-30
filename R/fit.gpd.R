e7219501eb6edc2b1ba7bd6b239fbeaa06ceb1ab
illtyd <robert.king@newcastle.edu.au>
2017-10-30 15:27:05 +1100

    code is if(FALSE) out, but will use the SLD results to calculate SEs if lambda4 is within all.equal of zero

--- R/fit.gpd.R
+++ R/fit.gpd.R
@@ -72,24 +72,63 @@ fit.gpd.lmom.given <- function(lmoms,n=NULL){
   betahatB <- l2*(lambdahatB+1)*(lambdahatB+2)
   alphahatA <- l1+(betahatA*(1-2*deltahatA))/(lambdahatA+1)
   alphahatB <- l1+(betahatB*(1-2*deltahatB))/(lambdahatB+1)
+  if (all.equal(lambdahatA,0)) { # lamdbahatA is close to zero
+    # Use SLD special case
+    AisSLD = TRUE
+    lambdahatA = 0 
+  } else {AisSLD = FALSE}
   lmomestA <- c(alphahatA,betahatA,deltahatA,lambdahatA)
-  if (gl.check.lambda(lmomestA,param="gpd")) {  names(lmomestA) <- c("alpha","beta","delta","lambda")} else {lmomestA <- NA}
+  if (gl.check.lambda(lmomestA,param="gpd")) {  
+    names(lmomestA) <- c("alpha","beta","delta","lambda")
+    RegionAest = TRUE} else {lmomestA <- NA
+    RegionAest = FALSE}
   lmomestB <- c(alphahatB,betahatB,deltahatB,lambdahatB)
   if (gl.check.lambda(lmomestB,param="gpd")) {  names(lmomestB) <- c("alpha","beta","delta","lambda")} else {lmomestB <- NA}
-  if (FALSE){ #if (!is.null(n)){ # Sample size is known - calculate Std Errors
+ if (FALSE){ # code which is iffed out below
+   # calculates SEs in the SLD special case, but not in 
+   # the main gld GPD case if (!is.null(n)){ # Sample size is known - calculate Std Errors
     # Calculate standard errors for gld
     # Check if SEs exist in region A (lambda > -0.5 )
     #om = dh*(1-dh) # omega
     #se.alpha = bh * sqrt((57 + (125*pi^2-1308)*om)/(15*n))
     #se.beta = bh * sqrt(4/(3*n) * (1 - (pi^2-8)*om))
     #se.delta = sqrt((8-(397+160*om-20*pi^2*(om+2))*om)/(15*n))
-    lmomse <- c(se.alpha,se.beta,se.delta)
-    ret <- cbind(lmomest,lmomse)
-    dimnames(ret) <- list(c("alpha","beta","delta"),
-                          c("Estimate","Std. Error"))
+    if (RegionAest){
+      # calculate SEs for region A estimate
+      # Don't do this test for region B, because they always exist?
+      if (AisSLD){
+        # lambda = 0, so this is the SLD - SE calculation ...
+        # see fit.R in R package, sld
+        warning("Since lambda estimate is zero, the estimated distribution\nis a special case, the Quantile Based Skew Logistic Distribution.\nNo standard errors are available for lambda, but SEs for the other\nparameters are given from the Quantile Based SLD.")
+        omega = lmomestA[3]*(1-lmomestA[3])
+        se.alpha = lmomestA[2] * sqrt((57 + (125*pi^2-1308)*omega)/(15*n))
+        se.beta = lmomestA[2] * sqrt(4/(3*n) * (1 - (pi^2-8)*omega))
+        se.delta = sqrt((8-(397+160*omega-20*pi^2*(omega+2))*omega)/(15*n))
+        lmomestA <- cbind(lmomestA,c(se.alpha,se.beta,se.delta,NA))
+        dimnames(lmomestA) <- list(c("alpha","beta","delta","lambda"),c("Estimate","Std. Error"))
+        
+      } else {warning("SE calculation not yet implemented")
+      }
+    } 
+      # calculate SEs for region B estimate
+      # If we have go this far, there is always a region B estimate?
+      if (lmomestB[[4]] == 0){
+        # lambda = 0, so this is the SLD - SE calculation ...
+        # see fit.R in R package, sld
+        warning("Since lambda estimate is zero, the estimate is a special case,\nthe Quantile Based Skew Logistic Distribution.  No standard errors are available for lambda,\nbut SEs for the other parameters are given from the Quantile Based SLD.")
+        omega = lmomestB$delta*(1-lmomestB$delta)
+        se.alpha = lmomestB$beta * sqrt((57 + (125*pi^2-1308)*omega)/(15*n))
+        se.beta = lmomestB$beta * sqrt(4/(3*n) * (1 - (pi^2-8)*omega))
+        se.delta = sqrt((8-(397+160*omega-20*pi^2*(omega+2))*omega)/(15*n))
+        lmomestB <- cbind(lmomestB,c(se.alpha,se.beta,se.delta,NA))
+        dimnames(lmomestB) <- list(c("alpha","beta","delta","lambda"),c("Estimate","Std. Error"))
+        
+      } else {warning("SE calculation not yet implemented")
+      }
   } else {
-    ret <- list(estA=lmomestA,estB=lmomestB,param="gpd") # return just the estimates
+    # return just the estimates
   }
+  ret <- list(estA=lmomestA,estB=lmomestB,param="gpd")
   class(ret) <- "GldGPDFit"
   ret
 }
