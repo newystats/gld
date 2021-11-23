1fcf22d7e2f92de6f59bf77476d6854ce0dcc22e
newystats <robert.king.newcastle@gmail.com>
2021-11-23 23:19:33 +1100

    Change email and website

--- src/gld.fm5.fx.c
+++ src/gld.fm5.fx.c
@@ -2,9 +2,8 @@
  * language.
  *
  * Copyright (C) Robert King 1993,2000,2001,2005,2006
- * robert.king@newcastle.edu.au
- * http://tolstoy.newcastle.edu.au/~rking/publ/software.html
- *
+ * robert.king.newcastle@gmail.com
+ * https://github.com/newystats/gld/ 
  * This program is free software; you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License as published by the Free 
  * Software Foundation; either version 2 of the License, or (at your option) 
